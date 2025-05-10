0a8f77e3a5c2db40e418bae42d4b9438cbfe9352
Lars <lorbeeren@proton.me>
2025-05-10 23:46:17 +0200

    jvm.cpp

--- jvm.cpp
+++ jvm.cpp
@@ -1,5 +1,9 @@
-//
-// Created by Lars on 10/05/2025.
-//
-
 #include "jvm.h"
+
+void jvm::load() {
+    JNI_GetCreatedJavaVMs(&vm, 1l, nullptr);
+
+    if (vm->GetEnv(reinterpret_cast<void **>(&env), JNI_VERSION_1_8) == JNI_EDETACHED) {
+        vm->AttachCurrentThreadAsDaemon(reinterpret_cast<void **>(&env), nullptr);
+    }
+}
